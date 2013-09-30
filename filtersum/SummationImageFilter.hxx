c0d9502e5c68bc10e5cbda1cf9105d3045c22a8d
yuan liu <adaptionist@gmail.com>
2013-09-30 00:53:52 -0700

    under test

--- filtersum/SummationImageFilter.hxx
+++ filtersum/SummationImageFilter.hxx
@@ -47,20 +47,66 @@ void SummationImageFilter< TInputImage, TOutputImage >::GenerateData()
 
   outputPtr->SetRegions(inputPtr->GetRequestedRegion());
   outputPtr->Allocate();
+
+  int sizeTotal = inputSize[0]*inputSize[1]*inputSize[2]; // size of image
+  int sizeSlice = inputSize[0]*inputSize[1];
+  int sizeRow =  inputSize[0];
+
+  float sum = 0;
+
+  float * temp = new float[sizeTotal];
+  float * temp_start = temp;
+
+  int offset[7];
+  offset[0] = -1; // -1,0,0
+  offset[1] = -1; // 0,-1,0
+  offset[2] = -1; // -1,-1,0
+  offset[3] = -1; // 0,0,-1
+  offset[4] = -1; // -1,0,-1
+  offset[5] = -1; // 0,-1,-1
+  offset[6] = -1; // -1,-1,-1
   
-  int total = inputSize[0]*inputSize[1]*inputSize[2];
+  int sign[7];
+  sign[0] = 1;
+  sign[1] = 1;
+  sign[2] = -1;
+  sign[3] = 1;
+  sign[4] = -1;
+  sign[5] = -1;
+  sign[6] = 1;
 
   const short int * in = inputPtr->GetBufferPointer();  
-  float * out = outputPtr->GetBufferPointer(); 
-
+  float * out;
+  float * out_center = outputPtr->GetBufferPointer();
 
   t1=clock();
 
-  for(int i = 0; i < total; i++)
+  for(int i = 0; i < sizeTotal; i++)
   {
-    *out = *in;
+    if (i >= sizeSlice) // if it's not at top slice
+    {
+      if(i % sizeSlice > 0) // if it's not the first voxel in the row
+      { 
+        for(int j = 0; j < 7; j++) // sum neighbour voxels
+	{
+          out = temp + offset[j];
+          sum+= sign[j] * (*out);  
+	}        
+        sum+=*in; // sum current voxel
+	*temp = sum; // set output
+      }      
+    }
+    else
+    {
+
+    }
+    temp++;
     in++;
-    out++;
+  }
+
+  for(int i=0; i < sizeTotal; i++, out_center++, temp_start++)
+  {
+    *out_center = *temp_start;
   }
 
   t2=clock();
