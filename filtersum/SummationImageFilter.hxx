d7de36a6870df12257641b8893f6b5c5c4e9c331
yuan liu <adaptionist@gmail.com>
2013-10-02 17:15:53 -0700

    linear version seems to be working

--- filtersum/SummationImageFilter.hxx
+++ filtersum/SummationImageFilter.hxx
@@ -35,6 +35,8 @@ void SummationImageFilter< TInputImage, TOutputImage >::GenerateData()
   clock_t t1,t2;
   float diff;
 
+  t1=clock();
+
   itkDebugMacro(<< "SummationImageFilter::GenerateData() called");
 
   InputImageConstPointer inputPtr  = this->GetInput();
@@ -43,7 +45,7 @@ void SummationImageFilter< TInputImage, TOutputImage >::GenerateData()
   //get size of image
   typename TInputImage::SizeType inputSize = inputPtr->GetLargestPossibleRegion().GetSize();
 
-  std::cout<<"input size: "<<inputSize[0]<<", "<<inputSize[1]<<", "<<inputSize[2]<<std::endl;
+  // std::cout<<"input size: "<<inputSize[0]<<", "<<inputSize[1]<<", "<<inputSize[2]<<std::endl;
 
   outputPtr->SetRegions(inputPtr->GetRequestedRegion());
   outputPtr->Allocate();
@@ -52,61 +54,141 @@ void SummationImageFilter< TInputImage, TOutputImage >::GenerateData()
   int sizeSlice = inputSize[0]*inputSize[1];
   int sizeRow =  inputSize[0];
 
-  float sum = 0;
+  const short int * in = inputPtr->GetBufferPointer(); // pointer to input
 
-  float * temp = new float[sizeTotal];
-  float * temp_start = temp;
+  float sum;			// stores voxel intensity
 
-  int offset[7];
-  offset[0] = -1; // -1,0,0
-  offset[1] = -1; // 0,-1,0
-  offset[2] = -1; // -1,-1,0
-  offset[3] = -1; // 0,0,-1
-  offset[4] = -1; // -1,0,-1
-  offset[5] = -1; // 0,-1,-1
-  offset[6] = -1; // -1,-1,-1
-  
-  int sign[7];
+  unsigned short int position; //   storing current positional information
+
+  int divSlice;
+  int remainderSlice;
+  int divRow;
+  int remainderRow;
+
+  float * out[8];  //pointers to output data
+
+  // convention - array index : postion (x,y,x)
+  // 0 : 0,0,0			
+  // 1 : -1,0,0
+  // 2 : 0,-1,0
+  // 3 : -1,-1,0
+  // 4 : 0,0,-1
+  // 5 : -1,0,-1
+  // 6 : 0,-1,-1
+  // 7 : -1,-1,-1
+
+  float sign[8]; // addition/subtraction sign
   sign[0] = 1;
   sign[1] = 1;
-  sign[2] = -1;
-  sign[3] = 1;
-  sign[4] = -1;
+  sign[2] = 1;
+  sign[3] = -1;
+  sign[4] = 1;
   sign[5] = -1;
-  sign[6] = 1;
-
-  const short int * in = inputPtr->GetBufferPointer();  
-  float * out;
-  float * out_center = outputPtr->GetBufferPointer();
-
-  t1=clock();
-
-  for(int i = 0; i < sizeTotal; i++)
+  sign[6] = -1;
+  sign[7] = 1;
+ 
+  for(int i = 0; i<8; i++)
   {
-    if (i >= sizeSlice) // if it's not at top slice
-    {
-      if(i % sizeSlice > 0) // if it's not the first voxel in the row
-      { 
-        for(int j = 0; j < 7; j++) // sum neighbour voxels
-	{
-          out = temp + offset[j];
-          sum+= sign[j] * (*out);  
-	}        
-        sum+=*in; // sum current voxel
-	*temp = sum; // set output
-      }      
-    }
-    else
-    {
-
-    }
-    temp++;
-    in++;
+    out[i] = outputPtr->GetBufferPointer(); // set output pointers
   }
 
-  for(int i=0; i < sizeTotal; i++, out_center++, temp_start++)
+  for(unsigned int i = 0; i < sizeTotal; i++)
   {
-    *out_center = *temp_start;
+    position = 0;
+
+    // get position info
+    divSlice = i / sizeSlice;   	     // slice
+    remainderSlice = i % sizeSlice; 
+    divRow = remainderSlice / sizeRow;       // row
+    remainderRow = remainderSlice % sizeRow; // column
+
+    if(divSlice >= 1)
+      position |= 1 << 2;       // set 3rd bit to 1 if it's not top slice
+    if(divRow >= 1)      
+      position |= 1 << 1;	// set 2nd bit to 1 if it's not 1st row
+    if(remainderRow >= 1)
+      position |= 1; 	        // set 1st bit to 1 if it's not 1st column
+    
+    sum = (float)(*in);		// get current voxel value
+    
+    switch(position)
+    {
+    case 7:			// 111 : sum all
+      for(int j = 1; j < 8; j++)
+      {   
+        sum += sign[j] * (*out[j]);  
+      }
+      break;
+    case 6:			// 110: sum all except 1st column 
+      for(int j = 1; j < 8; j++)
+      {   
+        if(j % 2 != 0)
+	  continue;
+        sum += sign[j] * (*out[j]);  
+      }
+      break;
+    case 5:			// 101: sum all except 1st row
+      for(int j = 1; j < 8; j++)
+      {   
+        if(j == 2 || j == 3 || j == 6 || j == 7)
+	  continue;
+        sum += sign[j] * (*out[j]);  
+      }
+      break;
+    case 4:			// 100: sum all except 1st row and 1st column
+      sum += sign[4] * (*out[4]);  
+      break;
+    case 3:			// 011: sum all except top slice
+      for(int j = 1; j < 4; j++)
+      {   
+        sum += sign[j] * (*out[j]);  
+      }
+      break;
+    case 2:	  		// 010: sum all except top slice and
+				// 1st column
+      sum += sign[2] * (*out[2]);
+      break; 
+    case 1:			// 001: sum all except top slice and
+				// 1st row
+      sum += sign[1] * (*out[1]);
+      break;
+    default:			// 000: sum nothing
+      break;
+    }
+
+    *out[0] = sum;		// set sum
+   
+    if(i >= 1)			// increment pointers
+    {
+      out[1]++;
+      if(i >= sizeRow)
+      {
+        out[2]++;
+        if( i >= sizeRow + 1)
+    	{
+    	  out[3]++;
+          if( i >= sizeSlice)
+    	  {
+    	    out[4]++;
+    	    if( i >= sizeSlice + 1)
+    	    {
+    	      out[5]++;
+    	      if( i >= sizeSlice + sizeRow)
+    	      {
+    	        out[6]++;
+    		if( i >= sizeSlice + sizeRow + 1)
+    	        {
+    	          out[7]++;
+    		}
+    	      }
+    	    }
+    	  }
+    	}
+      }
+    }
+    
+    out[0]++;
+    in++;
   }
 
   t2=clock();
