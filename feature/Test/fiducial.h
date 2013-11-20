ef3699bf735d7840c0426d2fe58bd52b1bbe5958
Bill Liu <adaptionist@gmail.com>
2013-11-20 06:52:00 -0800

    adde lml reader

---
+++ feature/Test/fiducial.h
@@ -0,0 +1,11 @@
+#ifndef FIDUCIAL_H
+#define FIDUCIAL_H
+
+struct Fiducial
+{
+    int id;
+    string name;
+    double[3] value;
+}
+
+#endif
---
+++ feature/Test/lmlFileReader.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+
+#include <fstream>
+#include <string>
+#include <map>
+#incluve <utility>
+
+#include "fiducial.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+  if(argc < 2)
+  {
+    cout<<"need: <file path>"<<endl;
+    return -1;
+  }
+
+  //set input file path
+  ifstream infile(argv[1]);
+  
+  map<int, Fiducial> markers;
+
+  int line = 0;
+  string dummyLine;
+	
+  while(infile.good())
+  {
+     
+     if(line == 0)
+     {
+       getline(infile, dummyLine); 	   
+     }
+ 
+     Fiducial newMarker;
+     infile >> newMarker.id >> newMarker.name >> newMarker.value[0] >> newMarker.value[1] >> newMarker.value[2];
+     
+     markers[newMarker.id] = newMarker;
+
+  }//end of ifstream
+
+
+  map<int,Fiducial>::iterator it = markers.begin();
+  cout << "map contains:\n";
+  for (it=markers.begin(); it!=markers.end(); ++it)
+    cout << it->first << " => " << (it->second).name << (it->second).value[0] << (it->second).value[1] << (it->second).value[2] << endl;
+
+  return 0;
+}
