c772f66f5defccce3c461566025cef27b62a0457
yuan liu <adaptionist@gmail.com>
2013-11-21 03:16:48 -0800

    added conversion pixel before calculating response vector

--- feature/Fiducial/FiducialTest.cpp
+++ feature/Fiducial/FiducialTest.cpp
@@ -52,26 +52,30 @@ int main(int argc, char **argv)
   for (it=markers.begin(); it!=markers.end(); ++it)
     cout << it->first << " => " << (it->second).name <<", " << (it->second).value[0] <<", "<< (it->second).value[1] <<", "<< (it->second).value[2] << endl;
 
-
-  int * target = new int[3*5];
-  for(int i = 0; i < 5; i++)
+  int numTarget = 1;
+  int * target = new int[3*numTarget];
+  for(int i = 0; i < numTarget; i++)
   {
     for(int j = 0; j < 3; j++)
     {
-      target[i*3 + j] = j + 10;
+      target[i*3 + j] = 0;
     }
   }
-  int * trainInput = new int[3*3];
-  for(int i = 0; i < 3; i++)
+  int * trainInput = new int[3*markers.size()];
+  int m = 0;
+  for (it=markers.begin(); it!=markers.end(); ++it)
   {
+    //convert mm to pixel using factor
+    double factor [3] = {10,10,10};
+    int * convertResult = convertToPixelLength((it->second).value, factor);
     for(int j = 0; j < 3; j++)
     {
-      trainInput[i*3 + j] = i;
+      trainInput[m*3 + j] = convertResult[j];
     }
+    m++;
   }
   int * response;
-  int numTrain = 3;
-  int numTarget = 5;
+  int numTrain = markers.size();
   getTrainingData(target, numTarget, trainInput, numTrain, response);
   cout<<"response:"<<endl;
   for(int i = 0; i < numTrain * numTarget; i++)
